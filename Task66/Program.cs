39619b29013633a59087e7ff7b677b87942d44ca
Anna <troshkova.anna@mail.ru>
2022-09-19 22:20:09 +0400

    64

---
+++ Task66/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class workhome
+{
+    public static void Main()
+    {
+        task66();
+    }
+     public static void task66()
+    {
+        Console.Write("Введите m = ");
+        int m = Convert.ToInt16(Console.ReadLine());
+        Console.Write("Введите n = ");
+        int n = Convert.ToInt16(Console.ReadLine());
+        int sum = 0;
+        for (int i = m; i <= n; i++)
+        {
+            sum += i;
+        }
+        Console.Write("Сумма натуральных чисел {0}", sum);
+    }
+}
