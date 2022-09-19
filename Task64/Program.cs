18894847d541b0db508517fd3932af38e8c80037
Anna <troshkova.anna@mail.ru>
2022-09-19 22:22:36 +0400

    64

---
+++ Task64/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class workhome
+{
+    public static void Main()
+    {
+        task64();
+    }
+
+    public static void task64()
+    {
+        Console.Write("Введите m = ");
+        int m = Convert.ToInt16(Console.ReadLine());
+        Console.Write("Введите n = ");
+        int n = Convert.ToInt16(Console.ReadLine());
+        for (int i = m; i <= n; i++)
+        {
+            Console.Write($"{i} \t");
+        }
+    }
+}
